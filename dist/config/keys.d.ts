34a0704093c73717f7fd926b6af9ee03bfda0aea
Ronnie Mathew <ronnie@bricksimple.com>
2019-07-30 15:12:13 -0400

    Removed the dist directory

--- dist/config/keys.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: {
-    mongoURI: string;
-    mongoOptions: {
-        useNewUrlParser: boolean;
-    };
-};
-export default _default;
--- dist/config/keys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    mongoURI: 'mongodb://ronnie:password@localhost/kf?authSource=admin',
-    mongoOptions: {
-        useNewUrlParser: true,
-    },
-};
-//# sourceMappingURL=keys.js.map
